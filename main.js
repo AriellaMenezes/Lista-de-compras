aff422d42ae7eb3b69c8905c6892980713286f12
Ariella Menezes <99842201+AriellaMenezes@users.noreply.github.com>
2023-03-31 15:16:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 let listaDeintens = []
+let itemAEditar
 
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
 const ulItens = document.getElementById("lista-de-itens")
 const ulItensComprados = document.getElementById("itens-comprados")
+const listaRecuperada = localStorage.getItem('listaDeItens')
+
+function atulizaLocalStorage() {
+    localStorage.setItem('listaDeItens', JSON.stringify(listaDeintens))
+}
+
+// (valores omitidos, 0, null, NaN, undefined, "", false) << retornam false
+
+if(listaRecuperada) {
+    listaDeintens = JSON.parse(listaRecuperada)
+    mostrarItem()
+} else {
+    listaDeintens = []
+}
+
+
+
 
 form.addEventListener("submit" , function(evento) {
     evento.preventDefault()
     salvarItem()
     mostrarItem()
     itensInput.focus()
-})
+});
 
 function salvarItem() {
     const comprasItem = itensInput.value 
@@ -23,7 +41,7 @@ function salvarItem() {
     listaDeintens.push({
         valor: comprasItem,
         checar: false
-    })
+    });
 }
     itensInput.value =''
 }
@@ -50,16 +68,18 @@ function mostrarItem(){
        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+            <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number (itemAEditar) ? 'disabled': ''}></input>
         </div>
+       
         <div>
+            ${index === Number (itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>
     `
 
     }
-    })
+    });
 
 
     const inputCheck = document.querySelectorAll('input[type ="checkbox"]')
@@ -69,9 +89,9 @@ function mostrarItem(){
             const valorDoElemento =  evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeintens[valorDoElemento].checar = evento.target.checked
             mostrarItem()
-        })
+        });
 
-    })
+    });
 
     const deletarObjetos = document.querySelectorAll(".deletar")
 
@@ -80,10 +100,33 @@ function mostrarItem(){
             const valorDoElemento =  evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeintens.splice(valorDoElemento,1)
             mostrarItem()
-        })
+        });
 
-    })
+    });
 
+
+    const editarItens = document.querySelectorAll(".editar")
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar =  evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
+        });
+
+    });
+
+
+    atulizaLocalStorage()
+
+}
+
+function salvarEdicao() {
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+    //console.log(itemEditado.value)
+    listaDeintens[itemAEditar].valor = itemEditado.value
+    console.log(listaDeintens)
+    itemAEditar = -1
+    mostrarItem()
 }
 
 
